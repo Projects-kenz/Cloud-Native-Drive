ae49dc7d728e28d79d877f2f506f57146f6b6dc6
kenz <kenzmazin67@gmail.com>
2025-08-30 10:19:21 +0530

    "project pusing to remote,(Readme left)"

---
+++ terraform-project-1/variable.tf
@@ -0,0 +1,11 @@
+variable "vpc-cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "sub1-cidr_block" {
+  default = "10.0.1.0/24"
+}
+
+variable "sub2-cidr_block" {
+  default = "10.0.2.0/24"
+}
